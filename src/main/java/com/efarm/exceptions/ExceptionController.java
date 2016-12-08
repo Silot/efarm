cb0ebe774e16e1d0c5052114d5de6b04da8a0cf8
Silot <aposnaml@teiser.gr>
2016-12-08 20:01:50 +0200

    exception controller

---
+++ src/main/java/com/efarm/exceptions/ExceptionController.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.efarm.exceptions;
+
+import javax.servlet.http.HttpServletRequest;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ *
+ * @author tolis
+ */
+@ControllerAdvice
+@EnableWebMvc
+public class ExceptionController {
+    
+    private Log logger = LogFactory.getLog(ExceptionController.class);
+
+    @ExceptionHandler(value = Exception.class)
+    public String handleError(HttpServletRequest req, Exception exception) {
+        logger.error("Request: " + req.getRequestURL() + " raised " + exception);
+        return "error";
+    }
+    
+    
+}
